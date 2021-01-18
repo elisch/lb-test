c8738ba41a6fcc24a21054e8dd87ca8520f9c50c
Elin Schenell <elinschenell@gmail.com>
2021-01-18 21:53:39 +0100

    Add spinner component

---
+++ src/components/spinner/spinner.component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {ActivityIndicator} from 'react-native';
+
+import styles from './spinner.styles';
+
+const Spinner = () => <ActivityIndicator style={styles.spinner} />;
+
+export default Spinner;
---
+++ src/components/spinner/spinner.styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  spinner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 30,
+  },
+});
+
+export default styles;
