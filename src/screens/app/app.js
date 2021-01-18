8b8131c475d489ab7c8a2ae5015536481434a6d4
Elin Schenell <elinschenell@gmail.com>
2021-01-18 21:55:48 +0100

    Add home screen
    Add home screen with list view to display ads.

---
+++ src/screens/app/app.js
@@ -0,0 +1,60 @@
+import React, {useEffect, useState, useCallback} from 'react';
+import {StatusBar, SafeAreaView, FlatList} from 'react-native';
+
+import Ad from '../../components/ad/ad.component';
+import Spinner from '../../components/spinner/spinner.component';
+import {getAds} from '../../api/ads.api';
+import styles from './app.styles';
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const renderListFooter = () => (isLoading ? <Spinner /> : null);
+  const keyExtractor = ({ad_id}) => ad_id.toString();
+  const renderItem = ({item}) => <Ad item={item} />;
+
+  useEffect(() => getMoreAds(0), [getMoreAds]);
+
+  const getMoreAds = useCallback(
+    (currentDataLength) => {
+      setIsLoading(true);
+      getAds(currentDataLength)
+        .then((newData) => {
+          if (newData) {
+            setData([...data, ...newData]);
+          }
+        })
+        .catch((error) => console.error(error))
+        .finally(() => setIsLoading(false));
+    },
+    [data],
+  );
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.container}>
+        {isLoading && data.length <= 0 ? (
+          <Spinner />
+        ) : (
+          <FlatList
+            contentContainerStyle={styles.list}
+            data={data}
+            renderItem={renderItem}
+            ListFooterComponent={renderListFooter}
+            onEndReached={() => getMoreAds(data.length)}
+            keyExtractor={keyExtractor}
+            showsVerticalScrollIndicator={false}
+            initialNumToRender={2}
+            maxToRenderPerBatch={4}
+            onEndReachedThreshold={2}
+            windowSize={5}
+            removeClippedSubviews
+          />
+        )}
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
---
+++ src/screens/app/app.styles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    backgroundColor: '#D8F8E5',
+  },
+});
+
+export default styles;
